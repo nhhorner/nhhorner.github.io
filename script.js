4d82bee913b7737071bdb2831cc8fc394d9e34f1
Neal Horner <thenealhorner@gmail.com>
2015-05-02 13:55:22 -0700

    Added dynamic content

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-var map = L.map('map', {zoomControl: false}).setView([37.789234, -122.361497], 13);
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/nhhorner.aaf9a69b/{z}/{x}/{y}.png', {maxZoom: 18}).addTo(map);
+var resize = function(){
+    //Set Width
+    var width = $( window ).width();
+    $("main").css("left", (width - 700)/2);
+
+    //Set Height
+    var height = $( window ).height();
+    $("#mapWrapper").height(height);
+    $("main").height(height);
+    console.log("The height of the main section: ", height);
+};
+$(document).ready(resize);
+$(window).resize(resize);
+
+$( "#email" ).append( "thenealhorner" + "@" + "gmail" + ".com");
+
+
+var data = [
+                {"name":"Python Email Sender", "url":"https://github.com/nealhorner/Python-Email-Sender", "image":"", "data":""},
+                {"name":"Python One Line FizzBuzz", "url":"https://github.com/nealhorner/Python-Email-Sender", "image":"", "data":""}
+                ];
+
+for (var i in data) {
+    $( "main" ).append( "<a class='projectLink center' class='center' href='" + data[i].url + "'><div class='project center'><h2>"+data[i].name+"</h2></div></a>" );
+}
+
+$( "#mapWrapper" ).append( '<img id="map" src="dabay13.jpeg">' );
