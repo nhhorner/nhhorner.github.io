2f85c85f2505bc2b348685aaf92ddd501f5dd540
nealhorner <thenealhorner@gmail.com>
2015-05-04 13:52:18 -0700

    commented out project section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $( "#email" ).append( "thenealhorner" + "[at]" + "gmail" + ".com");
-
+/*
 for (var i in data) {
     if (data[i].image.length > 1) {
         $( "main" ).append( "<a class='projectLink center' class='center' href='" + data[i].url + "'><div class='project center' style='background-image:url(" + data[i].image + "); background-repeat:no-repeat;'><h2>"+data[i].name+"</h2></div></a>" );
@@ -7,3 +7,4 @@ for (var i in data) {
         $( "main" ).append( "<a class='projectLink center' class='center' href='" + data[i].url + "'><div class='project center'><h2>"+data[i].name+"</h2></div></a>" );
     }
 }
+*/
