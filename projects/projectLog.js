d2ccb0eb863e540279ea02385c9c34a980aadfd2
Neal Horner <thenealhorner@gmail.com>
2015-07-09 18:59:40 -0700

    Added Jekyll

--- projects/projectLog.js
+++
@@ -1,4 +0,0 @@
-var data = [
-{"name":"Python Email Sender", "url":"http://nealhorner.com/projects/emailsender/", "image":"", "date":"1430614230"},
-{"name":"Python One Line FizzBuzz", "url":"http://nealhorner.com/projects/fizzbuzz/", "image":"", "date":"1430614245"}
-];
